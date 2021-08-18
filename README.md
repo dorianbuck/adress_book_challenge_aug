d431eef154b138acc15a30dbfcfa381f07ac963b
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-18 10:23:46 +0200

    added 1st instruction for cypress

---
+++ README.md
@@ -0,0 +1,11 @@
+# User Stories
+Create a new contact
+
+# As a user
+To stay in touch with my friends
+I would like to be able to create a contact for them in my address book
+List contacts 
+
+# As a user
+In order to contact my friends
+I would like to be able to view a list of my saved contacts
---
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -0,0 +1,12 @@
+// cyTest.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("user can create a contact", () => {
+  it("test", () => {
+    cy.visit("http://localhost:3001");
+  });
+});
