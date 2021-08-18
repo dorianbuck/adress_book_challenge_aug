acc4d0def0477ca97ebcf04666e48ef040fe782f
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-18 10:37:01 +0200

    added button and header, ran test with successful results

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -5,8 +5,10 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-describe("user can create a contact", () => {
-  it("test", () => {
-    cy.visit("http://localhost:3001");
-  });
-});
+
+describe('user can create a contact', () => {
+	it('test', () => {
+		cy.visit('http://localhost:3001')
+		cy.get('#add-contact').click()
+	})
+})
