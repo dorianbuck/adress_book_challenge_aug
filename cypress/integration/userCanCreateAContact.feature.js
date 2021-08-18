2545d7ca777d539b2e5655d24caec018d0078de0
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-18 12:54:47 +0200

    added submitbutton

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -5,16 +5,16 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-
-describe('user can create a contact', () => {
-	it('test', () => {
-		cy.visit('http://localhost:3001')
-    cy.get('#add-contact').click()
-    cy.get('#name').type('Thomas')
-		cy.get('#email').type('thomas@craft.se')
-		cy.get('#phone').type('0700 101010')
-		cy.get('#company').type('Craft Academy')
-		cy.get('#notes').type('Awesome coder')
-		cy.get('#twitter').type('@thomasochman')
-	})
-})
+describe("user can create a contact", () => {
+  it("test", () => {
+    cy.visit("http://localhost:3001");
+    cy.get("#add-contact").click();
+    cy.get("#name").type("Thomas");
+    cy.get("#email").type("thomas@craft.se");
+    cy.get("#phone").type("0700 101010");
+    cy.get("#company").type("Craft Academy");
+    cy.get("#notes").type("Awesome coder");
+    cy.get("#twitter").type("@thomasochman");
+    cy.get("#submit").click();
+  });
+});
